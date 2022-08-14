76a75a85b83b7848a74de1235ea294203c205783
kokohany655 <https://github.com/kokohany655>
2022-08-14 12:22:39 +0200

    edit 3 time

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -13,19 +13,20 @@ const validateTokenMiddleware = asyncHandler((
     
       // get authHeader
       const token= req.header('x-auth-token');
-     
-          const decode = jwt.verify(
-            token as string,
-            config.TOKEN_SECRET as unknown as string
-          );
-          if (decode) {
-            next();
-          } else {
-            // failed to authenticate user
-            next(new ApiError('Failed to authenticate user', 401));
+          if(token){
+            const decode = jwt.verify(
+              token as string,
+              config.TOKEN_SECRET as unknown as string
+            );
+            if (decode) {
+              next();
+            } else {
+              // failed to authenticate user
+              next(new ApiError('Failed to authenticate user', 401));
+            }
+          }else{
+            next(new ApiError('No token provided', 401));
           }
-        
-    
   })
   
   export default validateTokenMiddleware;
